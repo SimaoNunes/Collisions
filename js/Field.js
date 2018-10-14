99088c27c6fb40c49f767c24f97d8499e8ac8d1d
badjelix <miguel.ptfg@hotmail.com>
2018-10-14 22:01:53 +0100

    field is not defined

---
+++ js/Field.js
@@ -0,0 +1,39 @@
+/*Base e paredes da cena*/
+
+class Field extends THREE.Object3D{
+
+
+    constructor(x,y,x){
+        super();
+
+        this.position.x = x;
+        this.position.y = y;
+        this.position.z = z;
+    
+        'use strict'
+
+        material = new THREE.MeshBasicMaterial({color: 0x349627, wireframe: false});
+
+        this.add(new Base(0,0,0))
+    }
+
+
+    class Base{
+        constructor(x, y, z){
+            super();
+    
+            this.position.x = x;
+            this.position.y = y;
+            this.position.z = z;
+        
+            'use strict';
+    
+            geometry = new THREE.PlaneGeometry(40, 20)
+            mesh = new THREE.Mesh(geometry, material);
+            mesh.position.set(x, y, z);
+            this.add(mesh);
+        }
+    }
+
+    
+}
---
+++ js/projeto2.js
@@ -0,0 +1,102 @@
+/*Segunda entrega, Cena Simples Interativa com Câmara Móvel e Colisões*/
+
+var camera, scene, renderer, clock; // variaveis gerais relativas a animacao
+
+var camera1; // diferentes tipos de cameras
+
+var geometry, material, mesh;
+
+var delta; // variavel relativa a passagem do tempo
+
+function createScene() {
+    'use strict';
+
+    scene = new THREE.Scene();
+
+    field = new Field(0,0,0)
+
+    scene.add(new THREE.AxisHelper(20));
+    
+    createCamera1();
+
+    camera = camera1;
+
+    scene.add(camera);
+    scene.add(field);
+}
+
+function createCamera1() {
+    'use strict';
+    camera1 = new THREE.OrthographicCamera(
+        window.innerWidth / - 10,
+        window.innerWidth/ 10,
+        (window.innerHeight / 10) - 10,
+        (window.innerHeight / - 10) - 10,
+        0,
+        1000
+    );
+    
+    camera1.position.x = 0;
+    camera1.position.y = 30;
+    camera1.position.z = 0;
+}
+
+function onKeyDown(e) {
+    'use strict';
+    switch (e.keyCode) {
+    case 65: //A
+    case 97: //a
+        scene.traverse(function (node) {
+            if (node instanceof THREE.Mesh) {
+                node.material.wireframe = !node.material.wireframe;
+            }
+        });   
+    }
+}
+
+function onKeyUp(e) {
+    'use strict';
+    switch (e.keyCode) {
+    }
+}
+
+function onResize() {
+    'use strict';
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    if (window.innerHeight > 0 && window.innerWidth > 0) {
+        camera.aspect = window.innerWidth / window.innerHeight;
+        camera.updateProjectionMatrix();
+    }
+}
+
+function render() {
+    'use strict';
+    renderer.render(scene, camera);
+}
+
+function init() {
+    'use strict';
+    renderer = new THREE.WebGLRenderer({
+        antialias: true
+    });
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+    
+    createScene();
+
+    render();
+
+    window.addEventListener("resize", onResize);
+    window.addEventListener("keydown", onKeyDown);
+    window.addEventListener("keyup", onKeyUp);    
+}
+
+function animate() {
+    'use strict';
+
+    render();
+
+    camera.lookAt( scene.position );
+
+    requestAnimationFrame(animate);
+}
